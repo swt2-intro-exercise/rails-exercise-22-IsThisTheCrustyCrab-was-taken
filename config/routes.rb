4a63c639033290c02ce2ce49e3bb6d896721b603
Bekay1211 <bekay12.11@gmail.com>
2022-11-01 13:39:22 +0100

    uncommented two lines of code :v

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
