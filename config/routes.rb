7126a7672e743ef646a7bc6c1525c769ebc9bd0c
Bekay1211 <bekay12.11@gmail.com>
2022-11-01 18:15:03 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'author/new'
+  get 'authors/new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rails routes".
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+
+  get 'new_author' => 'authors#new'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
