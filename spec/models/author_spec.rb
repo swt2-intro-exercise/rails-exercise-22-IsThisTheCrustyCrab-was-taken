e21b089d3d2b63c97bb38ad290537aaaf7a44646
Bekay1211 <bekay12.11@gmail.com>
2022-11-02 13:23:51 +0100

    failing test #3 (generated model using rails, new, better tast, thanks copilot)

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  # check if author has first_name, last_name and homepage
-  it "should have first_name, last_name and homepage" do
-    author = Author.new
-    expect(author).to respond_to(:first_name)
-    expect(author).to respond_to(:last_name)
-    expect(author).to respond_to(:homepage)
+  #check if author initializes correctly
+  it "should have first name, last name, and homepage" do
+    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(author.first_name).to eq('Alan')
+    expect(author.last_name).to eq('Turing')
+    expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+  end
+  #check if author name is correct
+  it "should return full name" do
+    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(author.name).to eq('Alan Turing')
   end
 end
