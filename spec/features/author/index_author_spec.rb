6207f46ba8b8d50175de901accac58c2c2edd6eb
Bekay1211 <bekay12.11@gmail.com>
2022-11-02 14:46:36 +0100

    failing #6

---
+++ spec/features/author/index_author_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "Author index page", type: :feature do
+  #check if index page exists and has table of all authors containing name, homepage and link to author page
+  it "should exist at 'authors_path' and render withour error" do
+    # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
+    visit authors_path
+  end
+  it "should have a table with all authors" do
+    #create alan
+    @alan = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_text("Alan Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+    expect(page).to have_link("Show", :href => author_path(@alan))
+  end
+  #check if index page has link to create new author
+  it "should have a link to create a new author" do
+    visit authors_path
+    expect(page).to have_link("New", :href => new_author_path)
+  end
+end
