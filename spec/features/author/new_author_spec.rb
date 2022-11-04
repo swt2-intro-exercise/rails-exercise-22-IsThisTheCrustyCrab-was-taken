573d179ce21aabb6748f28f533e409642397c494
Bekay1211 <bekay12.11@gmail.com>
2022-11-05 00:02:58 +0100

    passing #8, this time without one random character in test that shouldnt have existed

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -41,5 +41,5 @@ describe "New author page", type: :feature do
       fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
       find('input[type="submit"]').click
       expect(page).to have_text("Last name can't be blank")
-    end∆
+    end
  end
