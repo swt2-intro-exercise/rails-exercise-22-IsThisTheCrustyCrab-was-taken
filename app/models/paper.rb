a8be6f0812b88f2791c53e0dc80fa2b425dfc2b6
Bekay1211 <bekay12.11@gmail.com>
2022-11-06 19:14:48 +0100

    passing #12

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,6 @@
 class Paper < ApplicationRecord
+  #validation for paper
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :venue, presence: true, length: { minimum: 1 }
+  validates :year, presence: true, length: { minimum: 1 }
 end
