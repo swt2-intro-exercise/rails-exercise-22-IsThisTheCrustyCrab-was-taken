303025cbea21f3b51924b9009877200398d149dd
Bekay1211 <bekay12.11@gmail.com>
2022-11-06 20:37:47 +0100

    passing #19

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -4,6 +4,12 @@ FactoryBot.define do
      last_name { "Turing" }
      homepage { "http://wikipedia.de/Alan_Turing" }
   end
+  #factory author2 to create grace hopper
+  factory :author2, class: Author do
+      first_name { "Grace" }
+      last_name { "Hopper" }
+      homepage { "http://wikipedia.de/Grace_Hopper" }
+  end
   #factory for author without last name
   factory :author_without_last_name, class: Author do
     first_name { "Alan" }
--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -9,5 +9,22 @@ describe "Show paper page", type: :feature do
     visit edit_paper_path(@paper)
     expect(page).to have_selector("select[multiple]")
   end
+  # select multiple authors and save paper
+  it "should select multiple authors and save paper" do
+    #create paper
+    @paper = FactoryBot.create :paper
+    visit edit_paper_path(@paper)
+    #create author Alan Turing and Grace Hopper
+    @alan = FactoryBot.create :author
+    @grace = FactoryBot.create :author2
+    visit edit_paper_path(@paper)
+    #select Alan Turing and Grace Hopper on multiple select box
+    find('select[multiple]').find(:option, @alan.name).select_option
+    find('select[multiple]').find(:option, @grace.name).select_option
+    #click submit button
+    find('input[type="submit"]').click
+    #check if paper has 2 authors
+    expect(@paper.authors.count).to eq(2)
+  end
 end
 
