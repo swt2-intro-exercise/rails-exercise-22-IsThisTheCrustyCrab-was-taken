e18a5fcd999240e9289012eda4aea2b89c9fdf43
Bekay1211 <bekay12.11@gmail.com>
2022-11-06 20:05:19 +0100

    failing #18

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -11,4 +11,15 @@ describe "Show paper page", type: :feature do
     expect(page).to have_text("Mind 49: 433-460")
     expect(page).to have_text("1950")
   end
+  #check if page has a list of author's full names
+  it "should display the paper's authors" do
+    #create paper
+    @paper = FactoryBot.create :paper
+    #create authors
+    @alan = FactoryBot.create :author
+    @paper.authors << @alan
+    @paper.save
+    visit paper_path(@paper)
+    expect(page).to have_text("Alan Turing")
+  end
 end
