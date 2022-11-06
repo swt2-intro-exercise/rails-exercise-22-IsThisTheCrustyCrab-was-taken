bc0553846c1471a9498f1ed110b9aa4b546bbb73
Bekay1211 <bekay12.11@gmail.com>
2022-11-06 18:28:16 +0100

    failing #10

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -11,5 +11,12 @@ describe "Show author page", type: :feature do
     expect(page).to have_text("Alan Turing")
     expect(page).to have_text("http://wikipedia.de/Alan_Turing")
   end
+  #check if page has link to delete author
+  it "should have a link to delete the author" do
+    #create alan
+    @alan = FactoryBot.create :author
+    visit author_path(@alan)
+    expect(page).to have_link("Delete", :href => author_path(@alan))
+  end
 end
 
