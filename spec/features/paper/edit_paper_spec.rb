52d67c5b4ef54a7f4517e3262716f19a96f8c813
Bekay1211 <bekay12.11@gmail.com>
2022-11-06 20:39:59 +0100

    failing #20

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -26,5 +26,41 @@ describe "Show paper page", type: :feature do
     #check if paper has 2 authors
     expect(@paper.authors.count).to eq(2)
   end
+  #check if user can change the title of a paper
+  it "should change the title of a paper" do
+    #create paper
+    @paper = FactoryBot.create :paper
+    visit edit_paper_path(@paper)
+    #change title of paper
+    fill_in 'paper[title]', with: 'New Title'
+    find('input[type="submit"]').click
+    #check if title was changed
+    @paper.reload
+    expect(@paper.title).to eq("New Title")
+  end
+  #check if user can change the venue of a paper
+  it "should change the venue of a paper" do
+    #create paper
+    @paper = FactoryBot.create :paper
+    visit edit_paper_path(@paper)
+    #change venue of paper
+    fill_in 'paper[venue]', with: 'New Venue'
+    find('input[type="submit"]').click
+    #check if venue was changed
+    @paper.reload
+    expect(@paper.venue).to eq("New Venue")
+  end
+  #check if user can change the year of a paper
+  it "should change the year of a paper" do
+    #create paper
+    @paper = FactoryBot.create :paper
+    visit edit_paper_path(@paper)
+    #change year of paper
+    fill_in 'paper[year]', with: '2019'
+    find('input[type="submit"]').click
+    #check if year was changed
+    @paper.reload
+    expect(@paper.year).to eq(2019)
+  end
 end
 
