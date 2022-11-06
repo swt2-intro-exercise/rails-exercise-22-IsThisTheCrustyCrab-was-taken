39b0eb961d608e660f6bd10c7a2f0c3edf90cfbe
Bekay1211 <bekay12.11@gmail.com>
2022-11-06 19:20:54 +0100

    passing #13

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,5 +2,6 @@ class Paper < ApplicationRecord
   #validation for paper
   validates :title, presence: true, length: { minimum: 1 }
   validates :venue, presence: true, length: { minimum: 1 }
-  validates :year, presence: true, length: { minimum: 1 }
+  #https://stackoverflow.com/questions/1089436/validate-integer-in-rails
+  validates :year, presence: true, numericality: { only_integer: true }
 end
